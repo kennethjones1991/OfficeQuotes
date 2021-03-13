94e2412224fca65307e4f4d23f8feeeee25c4e5a
Kenneth Jones <kennethjones1991@outlook.com>
2021-03-13 04:24:49 -0700

    Added character images; designed storyboard and connected views to view controller

--- OfficeQuotes/ViewController.swift
+++ OfficeQuotes/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var quoteTextView: UITextView!
+    @IBOutlet weak var characterLabel: UILabel!
+    @IBOutlet weak var characterImageView: UIImageView!
+    @IBOutlet weak var getQuoteButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getRandomQuote(_ sender: Any) {
+        
+    }
+    
 }
 
